d46d27a0ea3b1247dbf46a06a27d1d26247c47aa
LisandroAlmeida <lialmeida27@gmail.com>
2024-10-01 22:38:25 -0300

    desafio-Felipao-1

---
+++ desafio-felipao/index.js
@@ -0,0 +1,28 @@
+let name = "Lisandro";
+let xp = 10000;
+
+if (xp < 1000) {
+  console.log("Ferro");
+  console.log("O Herói de nome " + name + " é Ferro e está no nível de " + xp);
+} else if (xp <= 2000) {
+  console.log("Bronze");
+  console.log("O Herói de nome " + name + " é Bronze e está no nível de " + xp);
+} else if (xp <= 5000) {
+  console.log("Prata");
+  console.log("O Herói de nome " + name + " é Prata e está no nível de " + xp);
+} else if (xp <= 7000) {
+  console.log("Ouro");
+  console.log("O Herói de nome " + name + " é Ouro e está no nível de " + xp);
+} else if (xp <= 8000) {
+  console.log("Platina");
+  console.log("O Herói de nome " + name + " é Platina e está no nível de " + xp);
+} else if (xp <= 9000) {
+  console.log("Ascendente");
+  console.log("O Herói de nome " + name + " é Ascendente e está no nível de " + xp);
+} else if (xp <= 10000) {
+  console.log("Imortal");
+  console.log("O Herói de nome " + name + " é Imortal e está no nível de " + xp);
+} else {
+  console.log("Radiante");
+  console.log("O Herói de nome " + name + " é Radiante e está no nível de " + xp);
+}
